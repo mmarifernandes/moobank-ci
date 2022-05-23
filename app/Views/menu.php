3815ecb2066def9823b2b42e0290895ccf5316ab
mmarifernandes <marinafernandess103@gmail.com>
2022-05-23 19:31:56 -0300

    tela menu

--- app/Views/menu.php
+++ app/Views/menu.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo base_url('/assets/css/teste.css')?>">
-
+<script src="https://kit.fontawesome.com/84a7caccb6.js" crossorigin="anonymous"></script>
   <title>Document</title>
 </head>
 <body>
@@ -15,22 +15,35 @@
     <p>A</p>
   </nav>
   </div>
+
+  <div class="all">
 <div class="left">
 <div class="botao1">
+
   <button>
+  <i class="fa-solid fa-money-bill"></i>
+  <br>
+  <br>
     Pagamentos
   </button>
 
   <button>
+    <i class="fa-solid fa-sack-dollar"></i>
+    <br>
+    <br>
     Poupança
   </button>
 <button>
+  <i class="fa-solid fa-receipt"></i>
+  <br>
+  <br>
   Extrato
 </button>
 </div>
 
 <div class="transfer">
   <button>
+    <i class="fa-solid fa-money-bill-transfer"></i>
     Transferencia
   </button>
 </div>
@@ -51,10 +64,10 @@
 
 <div class="right">
 <div class="container">
-<h3>Olá Marina</h3>
+<h3>Olá, Marina</h3>
 <h4>Aqui estão suas últimas transações</h4>
 </div>
 </div>
-
+</div>
 </body>
 </html>
